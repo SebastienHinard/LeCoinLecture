228713808a9d936fe7541df8e0cc12f673166765
SebastienHinard <se.hinard@gmail.com>
2019-05-13 16:20:44 +0200

    ajout des routes

---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+//declaration de l'app
+var coinLectureApp = angular.module('coinLectureApp',['ngRoute']);
+//config des routes
+coinLectureApp.config(function($routeProvider){
+    $routeProvider
+    .when('/home',{
+        controller: 'homeController',
+        templateUrl : 'assets/partials/home.html'
+    })
+    // dans cette route, on récupère un paramêtre : id
+    .when('/category',{
+        controller: 'categoryController',
+        templateUrl: 'assets/partials/category.html'
+    })
+    .when('/cart',{
+        controller: 'cartController',
+        templateUrl: 'assets/partials/cart.html'
+    })
+    .when('/product',{
+        controller: 'productController',
+        templateUrl: 'assets/partials/product.html'
+    })
+    .otherwise({
+        redirectTo: '/home'
+    });
+});
+//controller home
+coinLectureApp.controller('homeController',function(){
+
+})
+//controller category
+.controller('categoryController',function(){
+
+})
+//controller cartController
+.controller('cartController',function(){
+
+})
+//controller productController
+.controller('productController',function(){
+
+});
