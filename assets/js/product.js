5d963a4de5d1544aab8d436d30a34a3cf2ea9b96
core.editor <andreeavillatte@gmail.com>
2019-05-15 09:59:16 +0200

    Details presque terminé

--- assets/js/product.js
+++ assets/js/product.js
@@ -5,6 +5,7 @@ coinLectureApp.controller('productController',function($scope, $routeParams, $ro
             $scope.Nom = $rootScope.books[$scope.index].Nom;
             $scope.Auteur = $rootScope.books[$scope.index].Auteur;
             $scope.Prix = $rootScope.books[$scope.index].Prix;
+            $scope.LienPhoto = $rootScope.books[$scope.index].LienPhoto;
             $scope.Categorie = $rootScope.books[$scope.index].Categorie;
             $scope.Description = $rootScope.books[$scope.index].Description;
 });
