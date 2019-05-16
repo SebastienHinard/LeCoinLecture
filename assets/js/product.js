27602110ffc316c39f81b00f0e09f1167b0cd2f9
core.editor <andreeavillatte@gmail.com>
2019-05-16 09:37:20 +0200

    panier fonctionne pas

--- assets/js/product.js
+++ assets/js/product.js
@@ -1,15 +1,18 @@
 //controller productController
-coinLectureApp.controller('productController',function($scope, $routeParams, $rootScope){
-            $scope.index=$routeParams.id;
-            $scope.ID = $rootScope.books[$scope.index].ID;
-            $scope.Nom = $rootScope.books[$scope.index].Nom;
-            $scope.Auteur = $rootScope.books[$scope.index].Auteur;
-            $scope.Prix = $rootScope.books[$scope.index].Prix;
-            $scope.LienPhoto = $rootScope.books[$scope.index].LienPhoto;
-            $scope.Categorie = $rootScope.books[$scope.index].Categorie;
-            $scope.Description = $rootScope.books[$scope.index].Description;
-            $scope.Rating = $rootScope.books[$scope.index].Rating;
-            $scope.NrPages = $rootScope.books[$scope.index].NrPages;
-            $scope.Format = $rootScope.books[$scope.index].Format;
-            $scope.Code = $rootScope.books[$scope.index].Code;
+coinLectureApp.controller('productController', function ($scope, $routeParams, $rootScope, $location) {
+    $scope.index = $routeParams.id;
+    $scope.ID = $rootScope.books[$scope.index].ID;
+    $scope.Nom = $rootScope.books[$scope.index].Nom;
+    $scope.Auteur = $rootScope.books[$scope.index].Auteur;
+    $scope.Prix = $rootScope.books[$scope.index].Prix;
+    $scope.LienPhoto = $rootScope.books[$scope.index].LienPhoto;
+    $scope.Categorie = $rootScope.books[$scope.index].Categorie;
+    $scope.Description = $rootScope.books[$scope.index].Description;
+    $scope.Rating = $rootScope.books[$scope.index].Rating;
+    $scope.NrPages = $rootScope.books[$scope.index].NrPages;
+    $scope.Format = $rootScope.books[$scope.index].Format;
+    $scope.Code = $rootScope.books[$scope.index].Code;
+    $scope.gotoUrl = function (url) {
+        $location.path(url);
+    };
 });
